b39f3d2de090620a0b66704c6f93ee33e1c7647c
Stephen Lead <stephen.lead@gmail.com>
2015-05-13 13:56:20 +1000

    Set up CRUD for posts, with rudimentary views

--- Gemfile
+++ Gemfile
@@ -13,5 +13,4 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'haml', '~> 4.0.6'
 gem 'devise', '~> 3.4.1'
-
-
+gem 'simple_form', '~> 3.1.0'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,52 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = "post #{@post.title} added successfully."
+      redirect_to @post
+    else
+      errors = []
+      @post.errors.full_messages.each do |msg|
+        errors << msg
+      end
+      flash.now[:error] = errors
+      render 'new'
+    end
+  end
+
+  def edit
   end
 
   def show
   end
 
+  def destroy
+    @post.destroy
+    flash[:notice] = "Post #{@post.title} deleted successfully."
+    redirect_to post_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+    #@post = post.friendly.find(params[:id])
+  end
 
 end
