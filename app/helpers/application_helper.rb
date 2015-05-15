f9663bbc23e2049c433fefd0828dc14cb7b92c9e
Stephen Lead <stephen.lead@gmail.com>
2015-05-15 15:48:43 +1000

    Highlight the current page in the navigation menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,13 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, options).render(content).html_safe
   end
 
+  # Highlight the current menu item in the navigation
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'current' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
 end
