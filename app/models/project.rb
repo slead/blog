7ceebef2fcf085e33687ee53a8d4769fc52f727a
Stephen Lead <stephen.lead@gmail.com>
2015-05-30 15:10:15 +1000

    Remove paperclip. Use hard-coded image URL instead
    too many problems with PaperClip/AWS

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,4 @@ class Project < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
