67f143a4c6b32db855582957b64cfe23c73be9d8
Stephen Lead <stephen.lead@gmail.com>
2019-02-06 19:27:30 +1100

    update sitemap

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,10 @@ ActiveRecord::Schema.define(version: 20150601005938) do
     t.datetime "updated_at"
     t.string   "slug"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: true do |t|
